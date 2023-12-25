cb6da393c57bb878e7222d7a82afb1ca9c81fc68
Eren Macit <ernmctt@gmail.com>
2023-12-25 21:18:00 +0300

    React 2-3

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import {Link} from "react-router-dom";
 
-export default function ProductCard(props) {
-    return (
-        <div className='product-card'>
-            <img src="https://m.media-amazon.com/images/I/817xnvwK0PL._AC_UF1000,1000_QL80_.jpg" />
-            <p>{props.name}</p>
-            <p>{props.price}</p>
-        </div>
-    )
+function ProductCard(props) {
+	console.log(props);
+	return (
+		<div class="card">
+			<img
+				src={props.product.thumbnail}
+				class="card-img-top img-fluid"
+				alt="..."
+			/>
+			<div class="card-body">
+				<h5 class="card-title">{props.product.title}</h5>
+				<p class="card-text">{props.product.description}</p>
+				<Link to={"/products?id=" + props.product.id} class="btn btn-primary">
+					Detail
+				</Link>
+				<button className="btn btn-danger">Sil</button>
+			</div>
+		</div>
+	);
 }
+
+export default ProductCard;
